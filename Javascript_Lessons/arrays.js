4e6734aa3875dbf6efa559b71a2b393511533a16
DiniOsman <83650577+DiniOsman@users.noreply.github.com>
2021-10-10 10:47:20 +0300

    Add files via upload

---
+++ Javascript_Lessons/arrays.js
@@ -0,0 +1,11 @@
+//const numbers = new Array(1, 2, 3, 4, 5);
+//document.write(numbers);
+
+
+const persons = ['dini', 'hassan', 'osman', 'yusuf', 'hassan']
+persons.push('ali')
+persons.unshift('yare');
+document.write(persons)
+console.log(Array.isArray('hello'))
+
+console.log(persons.indexOf('hassan'))
